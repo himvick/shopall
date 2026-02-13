8892e397dfae95063517a31df82050345c9e1068
Victor <grantv076@gmail.com>
2026-02-13 11:06:03 -0800

    added send ether functionality

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <div></div>
+      <div className="flex flex-row">
+        <div className="">S</div>
+        <p>ShopAll</p>
+      </div>
       <div></div>
     </div>
   );
